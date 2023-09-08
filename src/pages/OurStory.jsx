3fe005c830b272ac1f67bcaae32d78b78ea78315
WaiLinnAung16 <waia00198@gmail.com>
2023-09-08 13:17:01 +0630

    add gif

--- src/pages/OurStory.jsx
+++ src/pages/OurStory.jsx
@@ -1,92 +1,72 @@
-import React, { useEffect, useRef } from "react";
-import story1 from "/public/image0.jpeg";
-import tracks from "/public/tracks.png";
-import ReactPlayer from "react-player";
-import video from "/video.mp4";
-import { ControlBar, Player } from "video-react";
-
-import "../../node_modules/video-react/dist/video-react.css";
-
+import React from "react";
+import gif from "/terror.gif";
 const OurStory = () => {
   return (
     <div id="ourstory" className="bg-primary text-white pt-20 font-serif">
       <div className="container mx-auto">
-        <div className=" h-[500px] bg-primary/80 absolute text-center">
-          <h1 className="text-5xl font-mono font-bold mb-10">Our Story</h1>
-          <p className="w-full lg:w-[80%] mx-auto text-base md:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt qui
-            fugit dolorum magnam? Voluptatum est deleniti cupiditate quisquam
-            alias dolores ex earum dicta! Iure quisquam nemo dicta quos aperiam
-            sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-            qui fugit Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Sunt qui fugit dolorum magnam? Voluptatum est deleniti cupiditate
-            quisquam alias dolores ex earum dicta! Iure consectetur adipisicing
-            elit
+        <div className="text-center absolute h-[650px] bg-primary/60 backdrop-blur-sm">
+          <h1 className="text-5xl font-black text-white p-5 font-mono uppercase">
+            Our Story
+          </h1>
+          <p className="px-5 text-lg w-full md:w-[90%] mx-auto">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+            placeat sequi quae, fugit sed consectetur fugiat ex atque, quidem,
+            et unde deserunt natus! Cupiditate pariatur voluptatum dolorum.
+            Inventore laboriosam sunt dolores, quam consequatur fugit
+            reprehenderit quidem, molestiae iusto odio magni ea reiciendis
+            dolorem sint delectus modi aspernatur! Atque expedita rerum
+            voluptates nemo delectus corporis ipsa culpa esse optio eum iste
+            illum eveniet minus non natus corrupti pariatur repellat earum
+            aliquam consectetur, quam voluptatibus nobis! Voluptate iure
+            obcaecati et placeat molestiae fuga totam, ab deserunt dolore
+            eveniet accusantium nesciunt, dignissimos exercitationem officiis,
+            labore laudantium inventore repellendus aliquam iste minus dolor
+            blanditiis!
           </p>
         </div>
-        {/* <Player
-          autoPlay
-          src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-        >
-          <ControlBar autoHide={false} className="my-class" />
-        </Player> */}
-
-        <ReactPlayer
-          playing
-          loop
-          url={video}
-          controls={false}
-          width="100%"
-          height="500px"
-        />
+        <img src={gif} alt="" className="h-[650px] w-full object-cover" />
+        {/* <div className="grid grid-cols-12 mb-5">
+          <div className=" col-span-6">
+            <img
+              src={tracks}
+              alt=""
+              className="h-[250px] w-full px-5 object-cover"
+            />
+          </div>
+          <div className=" col-span-6">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis a
+              sint facere libero. Doloribus nemo quia deserunt perspiciatis
+              fuga, maiores molestiae omnis? Nisi sint, quas dicta ut itaque hic
+              iure recusandae accusantium! Maiores quaerat, rem repellendus
+              praesentium, voluptatum in, ipsa consectetur expedita eius sit
+              facere earum ut? Hic, illo veritatis?
+            </p>
+          </div>
+        </div>
+        <div className="grid grid-cols-12">
+          <div className=" col-span-6">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis a
+              sint facere libero. Doloribus nemo quia deserunt perspiciatis
+              fuga, maiores molestiae omnis? Nisi sint, quas dicta ut itaque hic
+              iure recusandae accusantium! Maiores quaerat, rem repellendus
+              praesentium, voluptatum in, ipsa consectetur expedita eius sit
+              facere earum ut? Hic, illo veritatis?
+            </p>
+          </div>
+          <div className=" col-span-6">
+            <img
+              src={tracks}
+              alt=""
+              className="h-[250px] w-full px-5 object-cover"
+            />
+          </div>
+        </div>
+        <ReactPlayer playing loop url={video} width="100%" height="650px" /> */}
       </div>
     </div>
   );
 };
 
 export default OurStory;
-
-{
-  /* 
-        <div className="grid grid-cols-6 gap-5 absolute bg-primary/50 backdrop-blur-sm">
-          <div className="col-span-6 lg:col-span-3">
-            <img
-              src={story1}
-              alt=""
-              className="h-[350px] w-full object-cover"
-            />
-          </div>
-          <div className="col-span-6 lg:col-span-3">
-            <h1 className="font-bold text-5xl font-mono mb-3">Something</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt qui
-              fugit dolorum magnam? Voluptatum est deleniti cupiditate quisquam
-              alias dolores ex earum dicta! Iure quisquam nemo dicta quos
-              aperiam sequi. Lorem ipsum dolor sit amet consectetur adipisicing
-              elit. Sunt qui fugit Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Sunt qui fugit dolorum magnam? Voluptatum est
-              deleniti cupiditate quisquam alias dolores ex earum dicta! Iure
-            </p>
-          </div>
-          <div className="col-span-6 lg:col-span-3 mt-5">
-            <h1 className="font-bold text-5xl font-mono mb-3">Something</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt qui
-              fugit dolorum magnam? Voluptatum est deleniti cupiditate quisquam
-              alias dolores ex earum dicta! Iure quisquam nemo dicta quos
-              aperiam sequi. Lorem ipsum dolor sit amet consectetur adipisicing
-              elit. Sunt qui fugit Lorem ipsum dolor sit amet consectetur
-              adipisicing elit.
-            </p>
-          </div>
-          <div className="col-span-6 lg:col-span-3">
-            <img
-              src={tracks}
-              alt=""
-              className="h-[300px] w-full object-cover"
-            />
-          </div>
-        </div>
-
-        <ReactPlayer playing loop url={video} width="100%" height="650px" /> */
-}
